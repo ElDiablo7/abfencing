3996f213076498f7b1cbb1990f7ba44c6d62344b
ElDiablo7 <118941866+ElDiablo7@users.noreply.github.com>
2026-09-18 16:53:57 +0100

    Make sitemap host-aware for Railway

--- server.js
+++ server.js
@@ -132,7 +132,10 @@ app.post('/api/quote', async (req, res) => {
 });
 
 app.get('/robots.txt', (req, res) => res.sendFile(path.join(__dirname, 'public', 'robots.txt')));
-app.get('/sitemap.xml', (req, res) => res.sendFile(path.join(__dirname, 'public', 'sitemap.xml')));
+app.get('/sitemap.xml', (req, res) => {
+  const origin = `${req.protocol}://${req.get('host')}`;
+  res.type('application/xml').send(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>${origin}/</loc></url></urlset>`);
+});
 app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.listen(port, '0.0.0.0', () => console.log(`AB Fencing site listening on ${port}`));
